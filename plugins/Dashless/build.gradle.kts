b5da65af170dd5536844b87ed449eb8ef3951116
Winston Sepruko <notexactlymebutmeh@gmail.com>
2021-09-28 21:10:18 +1000

    Dashless will now appear in updater.json

--- plugins/Dashless/build.gradle.kts
+++ plugins/Dashless/build.gradle.kts
@@ -1,7 +1,3 @@
 version = "1.0.0"
 description =
     "Replaces dashes in channel names with spaces. This is likely to break on Discord updates!"
-
-aliucord {
-    excludeFromUpdaterJson.set(true)
-}
